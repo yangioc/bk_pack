3e355bf4c6eba880d7117408102715299564cb2c
yang <yang@gmail.com>
2023-11-06 21:22:05 +0800

    add task cron

---
+++ crontab/ticker.go
@@ -0,0 +1,89 @@
+package schedulerTicker
+
+import (
+	"fmt"
+
+	"github.com/robfig/cron/v3"
+	"github.com/rs/xid"
+)
+
+type Task struct {
+	Name     string
+	EntryID  cron.EntryID
+	Function func()
+	Spec     string
+}
+
+type SchedulerTicker struct {
+	Cron *cron.Cron
+	Jobs *tickerBroadcast
+}
+
+func Init() *SchedulerTicker {
+	return &SchedulerTicker{
+		Cron: cron.New(cron.WithSeconds()),
+		Jobs: new(tickerBroadcast),
+	}
+}
+
+func (s *SchedulerTicker) Run() {
+	s.Cron.Run()
+}
+
+// 每幾秒，無限迴圈執行,
+// secondTime: 每幾秒可以執行,
+// function: 要執行的 function
+func (s *SchedulerTicker) NewIntervalTask(name, spec string, function func()) error {
+	if name == "" {
+		name = xid.New().String()
+	}
+
+	if _, exist := s.Jobs.Load(name); !exist {
+		entryId, err := s.Cron.AddFunc(spec, function)
+		if err != nil {
+			return err
+		}
+
+		task := &Task{
+			Name:     name,
+			EntryID:  entryId,
+			Function: function,
+			Spec:     spec,
+		}
+		s.Jobs.Store(task.Name, task)
+	} else {
+		return fmt.Errorf("this taskName: %v have been used", name)
+	}
+
+	return nil
+}
+
+// 中斷任務,
+// eventName: 要中斷的任務 id
+func (s *SchedulerTicker) DeleteTask(taskName string) {
+	if task, exist := s.Jobs.Load(taskName); exist {
+		s.Cron.Remove(task.EntryID)
+		s.Jobs.Delete(task.Name)
+	}
+}
+
+func (s *SchedulerTicker) ClearAllTask() {
+	s.Jobs.Range(func(key string, t *Task) bool {
+		s.Cron.Remove(t.EntryID)
+		s.Jobs.Delete(t.Name)
+		return true
+	})
+}
+
+func (s *SchedulerTicker) GetTask(taskName string) (t *Task, exist bool) {
+	return s.Jobs.Load(taskName)
+}
+
+func (s *SchedulerTicker) GetAllTasks() []*Task {
+	tList := make([]*Task, 0)
+	s.Jobs.Range(func(key string, t *Task) bool {
+		tList = append(tList, t)
+		return true
+	})
+	return tList
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/yangioc/bk_pack
 go 1.20
 
 require (
+	github.com/bwmarrin/snowflake v0.3.0
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/json-iterator/go v1.1.12
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/rs/xid v1.5.0
 	golang.org/x/crypto v0.14.0
 	golang.org/x/text v0.14.0
@@ -13,7 +15,6 @@ require (
 )
 
 require (
-	github.com/bwmarrin/snowflake v0.3.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/sys v0.13.0 // indirect
