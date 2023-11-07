daf056e3cfe3c7a1dd41c298c327d6644bf0535e
yang <yang@gmail.com>
2023-11-07 18:56:22 +0800

    add log 加入字串轉level

--- log/constant.go
+++ log/constant.go
@@ -8,11 +8,11 @@ var Level uint8 = 255
 // 0: Test log 不管輸出等級一定會輸出
 var TagMap map[uint8]struct{} = map[uint8]struct{}{}
 
-// var levelToStringMap = map[uint8]string{
-// 	Level_Error: "error",
-// 	Level_Info:  "info",
-// 	Level_Debug: "debug",
-// }
+var LevelToStringMap = map[string]uint8{
+	"error": Level_Error,
+	"info":  Level_Info,
+	"debug": Level_Debug,
+}
 
 const (
 	Level_Error = 0
